d552071d65e30e79c5bccc8e974af53a05f032e2
Annia Sebold <anniasebold@pop-os.localdomain>
2021-05-16 12:20:28 -0300

    1175

--- fun-vet/troca-vetor.py
+++ fun-vet/troca-vetor.py
@@ -1,5 +1,22 @@
-def lerValores():
-  n = []
-  for i in range(0, 20):
-    x.append(int(input()))
-  return n
+def troca(v):
+  j = 19
+  for i in range(10):
+    temp = v[j]
+    v[j] = v[i]
+    v[i] = temp
+    j = j -1
+
+def imprime(v):
+  for i in range(20):
+    print(f"N[{i}] = {v[i]}")
+
+def main():
+  vet = []
+  for i in range(20):
+    num = int(input())
+    vet.append(num)
+    
+  troca(vet)
+  imprime(vet)
+
+main()
